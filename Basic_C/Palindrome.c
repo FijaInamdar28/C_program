cd3ea1180e51f4e6f407a3628904f8b332fea58b
INAMDAR FIJA <87868483+FijaInamdar28@users.noreply.github.com>
2022-04-14 00:01:44 +0530

    Update Palindrome.c

--- Basic_C/Palindrome.c
+++ Basic_C/Palindrome.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
 
+int main()
+{
+    int n,r,sum=0,temp;
+    printf("Please Enter the Number:\n");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        r=n%10;
+        sum=(sum*10)+r;
+        n=n/10;
+    }
+    if(temp==sum)
+    {
+        printf("Number is Palindrome\n");
+    }
+    else
+    {
+        printf("Number is Not Palindrome\n");
+
+    }
+        
+    return 0;
+
+    
+}
